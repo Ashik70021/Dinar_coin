cfe80c2cfa8a21d0d95620727751a4fb9e2a72cd
Ashik70021 <ashik70021@gmail.com>
2025-01-20 19:09:00 +0600

    fix marque issue

--- src/components/Marq.jsx
+++ src/components/Marq.jsx
@@ -3,22 +3,24 @@ import logo4 from '../../public/image/logo4.jpg'
 const Marq = () => {
     return (
         <div className="">
-            <Marquee>
-                <div className="w-full border-t-4 border-b-4 bg-[#000000] border-[#f3b841]">
-                    <div className="overflow-hidden ">
-                        <div className="flex items-center space-x-6 py-6">
+
+            <div className="w-full border-t-4 border-b-4 bg-[#000000] border-[#f3b841]">
+                <div className="overflow-hidden ">
+                    <div className="flex items-center space-x-6 py-6">
+                        <Marquee>
                             <img src={logo4} alt="Marquee Image" className="h-32 w-32 object-cover rounded-[50%]" />
-                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold">DINAR COIN</p>
+                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold pr-8">DINAR COIN</p>
                             <img src={logo4} alt="Marquee Image" className="h-32 w-32 object-cover rounded-[50%]" />
-                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold">JOIN THE COMMUNITY</p>
+                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold pr-8">JOIN THE COMMUNITY</p>
                             <img src={logo4} alt="Marquee Image" className="h-32 w-32 object-cover rounded-[50%]" />
-                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold">ARAB COIN</p>
+                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold pr-8">ARAB COIN</p>
                             <img src={logo4} alt="Marquee Image" className="h-32 w-32 object-cover rounded-[50%]" />
-                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold">JOIN THE COMMUNITY</p>          
-                        </div>
+                            <p className="text-5xl font-extrabold text-[#d99e2a] font-semibold pr-8">JOIN THE COMMUNITY</p>
+                        </Marquee>
                     </div>
                 </div>
-            </Marquee>
+            </div>
+
         </div>
     );
 };
